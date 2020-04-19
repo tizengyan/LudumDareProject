cdbb3853775ccf21840edcb97f5ef86b15e8a3d1
Potato Wu <z.wu233@gmail.com>
2020-04-19 14:00:09 +0800

    Start game to Tutorial UI should be working in ArtScene

---
+++ Assets/Scripts/UI_MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI_MainMenuController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject swipeMenu;
+    public void LoadSwipeScene()
+    {
+        swipeMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI_SwipeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_SwipeController : MonoBehaviour
+{
+    public Animator characterAnimator;
+    public GameObject tutorialMenu;
+
+    public int previousCharacterIndex;
+
+    public void swipeNo ()
+    {
+        characterAnimator.SetTrigger("TriggerNo");
+    }
+
+    public void swipeYes()
+    {
+        characterAnimator.SetTrigger("TriggerYes");
+        StartCoroutine(StartTutorial());
+    }
+
+    IEnumerator StartTutorial()
+    {
+        yield return new WaitForSeconds(1f);
+        tutorialMenu.SetActive(true);
+    }
+
+    private void Update()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        if (tutorialMenu.activeSelf && Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Start game!");
+        }
+    }
+}
