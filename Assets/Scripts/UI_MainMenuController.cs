4a9b2bcb31f523c21974478e564ab7296c4dac87
Potato Wu <z.wu233@gmail.com>
2020-04-19 16:53:37 +0800

    A UI Canvas prefab, pausing/resuming game

--- Assets/Scripts/UI_MainMenuController.cs
+++ Assets/Scripts/UI_MainMenuController.cs
@@ -6,9 +6,58 @@ public class UI_MainMenuController : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject swipeMenu;
+    public GameObject pauseMenu;
+    public GameObject creditsPage;
+
+    public bool isPaused;
+
     public void LoadSwipeScene()
     {
         swipeMenu.SetActive(true);
         mainMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();                   
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenu.SetActive(false) ;
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsOpen()
+    {
+        creditsPage.SetActive(true);
+    }
+
+    public void CreditsClose()
+    {
+        creditsPage.SetActive(false);
     }
 }
