b35a76a77aa55f6560f42f53abc21fd3b1f27912
Tizeng Yan <yan235689147@gmail.com>
2020-04-19 10:55:35 +0800

    add arts to main scene

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    [SerializeField]
+    private float selfDestroyDelay = -1f;
+    [SerializeField]
+    private float speed = -1f;
+
+    void Start() {
+
+    }
+    
+    void Update() {
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,9 @@ public class PlayerController : MonoBehaviour {
     private float jumpStrength = 1f;
 
     bool gameIsOver = false;
+    bool isGrounded = false;
     Rigidbody2D rb2d;
+    Animator animator;
 
     public void setGameIsOver(bool isOver) {
         gameIsOver = isOver;
@@ -15,7 +17,9 @@ public class PlayerController : MonoBehaviour {
 
     void Start() {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
         gameIsOver = false;
+        Invoke("run", 2);
     }
 
     // Update is called once per frame
@@ -27,11 +31,27 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    void run() {
+        animator.SetBool("isRunning", true);
+        animator.SetBool("isJumping", false);
+    }
+
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Obstacle") {
             GameManager.GetInstance().gameOver();
             // stop obstacle and self
             collision.gameObject.GetComponent<Obstacle>().stopMovement();
+        }
+        else if (collision.gameObject.tag == "Ground") {
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Ground") {
+            isGrounded = false;
+            animator.SetBool("isJumping", true);
         }
     }
 }
