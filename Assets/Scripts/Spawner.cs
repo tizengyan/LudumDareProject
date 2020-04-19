40ee70123ece036d1bb35b52ddc28987477b00b1
Tizeng Yan <yan235689147@gmail.com>
2020-04-19 12:32:33 +0800

    add spawn points to two

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
     [SerializeField]
-    public GameObject[] obstaclePrefabs;
+    private GameObject[] spawnPoints;
+    [SerializeField]
+    private GameObject[] obstaclePrefabs;
+    [SerializeField]
+    private float delayMin = 1f, delayMax = 3f;
 
     float spawnDelay = 1.5f;
     float nextSpawnTime;
@@ -20,13 +24,20 @@ public class Spawner : MonoBehaviour {
     
     void Update() {
         if (!gameIsOver && Time.time > nextSpawnTime) {
-            spawnDelay = Random.Range(1f, 3f);
+            spawnDelay = Random.Range(delayMin, delayMax);
+
             nextSpawnTime = Time.time + spawnDelay;
-            int obIndex = Random.Range(0, obstaclePrefabs.Length - 1);
-            Debug.Log(obIndex);
-            Debug.Log(obstaclePrefabs[obIndex]);
-            if (obstaclePrefabs[obIndex]) {
-                Instantiate(obstaclePrefabs[obIndex]);
+            int spIndex = Random.Range(0, spawnPoints.Length);
+            if (spawnPoints.Length > 0) {
+                Debug.Log(spIndex);
+                Debug.Log(obstaclePrefabs[spIndex]);
+                int obIndex = Random.Range(0, obstaclePrefabs.Length);
+                Instantiate(obstaclePrefabs[spIndex], spawnPoints[spIndex].transform.position, Quaternion.identity);
+                //if (obstaclePrefabs.Length > 0) {
+                //    Debug.Log(obIndex);
+                //    Debug.Log(obstaclePrefabs[obIndex]);
+                //    Instantiate(obstaclePrefabs[obIndex], spawnPoints[spIndex].transform.position, Quaternion.identity);
+                //}
             }
         }
     }
