02a938769871c993040328ee7c4e1fd637141419
Potato Wu <z.wu233@gmail.com>
2020-04-20 11:47:34 +0800

    Added a Canvas_HUD prefab

---
+++ Assets/HPImageReplace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPImageReplace : MonoBehaviour
+{
+    public Image image;
+    public Sprite[] hpImage;
+    public int currentHealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.sprite = hpImage[currentHealth];
+    }
+}
