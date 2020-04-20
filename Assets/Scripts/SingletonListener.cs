ce0a724289f3cb7208a2d619d95ebd152e7093ca
Tizeng Yan <yan235689147@gmail.com>
2020-04-21 01:42:27 +0800

    add singleton listener

---
+++ Assets/Scripts/SingletonListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonListener : MonoBehaviour {
+    public static SingletonListener instance = null;
+
+    private void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
